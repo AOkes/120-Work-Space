724370490ea84b6423733ed0a27411986f83f669
Aidan Okes <AOkes@users.noreply.github.com>
2018-09-13 00:02:46 -0600

    Part One commit

---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+// Declaring the setup function
+function setup() {
+
+    // used to create a canvas that is 600 by 400 pixels
+    createCanvas( 600, 400 );
+
+    // used to color the background green
+    background( 'green' );
+}
+
+// Declaring the draw function
+function draw() {
+
+  // telling p5 to print Hello World! on the screen
+  // Position of the text will be at (x: 40, y: 40).
+  text( "Hello World!", 40, 40);
+}
